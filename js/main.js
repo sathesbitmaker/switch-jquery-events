770fea3bb690f05662cdaa9ec824f8696e9c7173
Sathes Balasingam <sathes352@gmail.com>
2017-02-15 17:23:19 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+$(function() {
+  $('.light').css('background-color', 'yellow')
+})
 
 
+$(function() {
+$('button').on('click', function() {
+if ($("button").hasClass('off')){
+  $( '.off').addClass('on')
+  $( '.off').removeClass('off')
+  $('.status').text('It is so bright in here!')
+$('.light').css('background-color', 'yellow')
+}
+  else {
+    $('.on').addClass('off')
+    $('.on').removeClass('on')
+    $('.status').text('It is so dark in here!')
+    $('.light').css('background-color', 'black')
+  }
+})
+})
